def73f68a0b1c839fe47d9ee71d00d27bb079514
raggastan <29949772+Raggastan@users.noreply.github.com>
2022-02-18 01:53:24 +0100

    Blink LED

--- LED/blink.ino
+++ LED/blink.ino
@@ -1,9 +1,13 @@
-void setup() {
-  
+const int ledPin = 2;    // The LED is plugged on pin 2
 
+void setup() {
+  pinMode(ledPin, OUTPUT);   // Just saying this is an output
+  digitalWrite(ledPin, LOW); // Set to off the LED
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  delay(500);
 }
