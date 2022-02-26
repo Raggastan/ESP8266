fcb65ec317a6930eccfc65b90496cacf2d44592a
raggastan <29949772+Raggastan@users.noreply.github.com>
2022-02-26 22:20:50 +0100

    Added secondary output

--- Tilt/main.ino
+++ Tilt/main.ino
@@ -1,23 +1,26 @@
 const int ledPin = 5;
 const int tiltPin = 16;
+const int laserPin = 4;
 
 void setup() {
     pinMode(ledPin, OUTPUT);
     digitalWrite(ledPin, LOW);
     pinMode(tiltPin, INPUT);
-    Serial.begin(115200);
+    pinMode(laserPin, OUTPUT);
+    digitalWrite(laserPin, LOW);
+    Serial.begin(9600);
     Serial.print("Started");
 }
 
 void loop() {
     int sensorValue = digitalRead(tiltPin);
-    Serial.print(sensorValue);
-    Serial.print(" ");
     if (sensorValue == HIGH) {
         digitalWrite(ledPin, HIGH);
+        digitalWrite(laserPin, HIGH);
     }
     else {
         digitalWrite(ledPin, LOW);
+        digitalWrite(laserPin, LOW);
     }
     delay(250);
 }
