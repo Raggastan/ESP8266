72ae2ed04b7f9a701ebf78a72955eab90ddb1354
raggastan <29949772+Raggastan@users.noreply.github.com>
2022-02-27 14:22:17 +0100

    Added HelloWifi project

---
+++ Wifi/HelloWifi.ino
@@ -0,0 +1,26 @@
+#include "Arduino.h"
+#include "ESP8266WiFi.h"
+
+#define PIN_LED 5
+const char * SSID = "RaggaPhone";
+const char * PASSWORD = "charcuterie";
+
+void setup() {
+    pinMode(PIN_LED, OUTPUT);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(SSID, PASSWORD);
+    Serial.begin(9600);
+    Serial.println("");
+}
+
+void loop() {
+    if (WiFi.isConnected()) {
+        digitalWrite(PIN_LED, HIGH);
+        Serial.println("Connected");
+    }
+    else {
+        digitalWrite(PIN_LED, LOW);
+        Serial.println("Offline");
+    }
+    delay(250);
+}
