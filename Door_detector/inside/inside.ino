b816b00d42a911ce8f9c684b6f53ee2f6ac17dcc
Raggastan <lincarvillais@gmail.com>
2022-10-23 17:37:35 +0200

    Added setup function

--- Door_detector/inside/inside.ino
+++ Door_detector/inside/inside.ino
@@ -1,4 +1,34 @@
 #include <stdlib.h>
 #include <ESP8266WiFi.h>
+#include <stdbool.h>
 
-#ifndef MOTION_SENSOR_PIN 
+#ifndef MOTION_SENSOR_PIN 10
+
+const char *ssid = "esp8266Wifi";
+const char *password = "M3@J;r5.-zX!n";
+
+bool motion_detected();
+void send_notification();
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Liaison série démarrée");
+  delay(10);
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+}
+
+void loop()
+{
+
+}
